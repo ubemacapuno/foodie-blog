68f1a49610522080d5d3b8a517a8a4d37d88bbc6
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-09-23 00:24:35 -0700

    Added Tailwind CSS and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/dishes.ejs',
+    './views/edit.ejs',
+    './views/index.ejs',
+    './views/ingredients.ejs',
+    './views/login.ejs',
+    './views/signup.ejs',
+    './src/**/*.{html,js}',
+  ],
+  // ...
+}
