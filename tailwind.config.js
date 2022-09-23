e91cd0e40c6ab1da8e89a41c18ea4d2af93101d4
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-09-23 11:39:16 -0700

    Added tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,34 @@ module.exports = {
     './views/signup.ejs',
     './src/**/*.{html,js}',
   ],
-  // ...
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"'],
+    },
+    // set breakpoints for screens:
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      //custom colors (similar to CSS variables):
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      }
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
 }
